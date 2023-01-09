3bb91640929875eff445c280f37b601ec9748e91
Алексей Дмитриев <tthhhuy28@gmail.com>
2023-01-09 13:16:05 +0300

    creating listener

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,18 @@
 use std::{
     collections::HashMap,
     env,
+    io::Error as IoError,
     net::SocketAddr,
     sync::{Arc, Mutex},
+    vec::Vec,
 };
 
 use futures_channel::mpsc::{unbounded, UnboundedSender};
+use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
 
 use http::header::{HeaderValue, SEC_WEBSOCKET_PROTOCOL};
+
+use serde::{Deserialize, Serialize};
 
 use tokio::net::{TcpListener, TcpStream};
 use tungstenite::{
@@ -16,12 +21,19 @@ use tungstenite::{
 };
 
 type Sender = UnboundedSender<Message>;
-
 struct PeerStruct {
     protocol: HeaderValue,
     sender: Sender,
 }
 type PeerMap = Arc<Mutex<HashMap<SocketAddr, PeerStruct>>>;
+
+async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, client_addr: SocketAddr) {
+    println!(
+        "Incoming TCP connection from: {}, raw stream: {}",
+        client_addr,
+        raw_stream.local_addr().unwrap()
+    );
+}
 
 #[tokio::main]
 async fn main() {
@@ -33,4 +45,9 @@ async fn main() {
     let try_socket = TcpListener::bind(&server_addr).await;
     let listener = try_socket.expect("Failed to bind");
     println!("Listening on: {}", server_addr);
+
+    while let Ok((stream, client_addr)) = listener.accept().await {
+    }
+
+    Ok(())
 }
