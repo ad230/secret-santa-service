c757f5ae879694b06c69ba8635e96af8dc7d8944
ad230 <aleshadmi@mail.ru>
2023-01-09 12:30:22 +0300

    peer realisation

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,32 @@
 use std::{
+    collections::HashMap,
     env,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
 };
+
+use futures_channel::mpsc::{unbounded, UnboundedSender};
+
+use http::header::{HeaderValue, SEC_WEBSOCKET_PROTOCOL};
+
+use tokio::net::{TcpListener, TcpStream};
+use tungstenite::{
+    handshake::server::{ErrorResponse, Request, Response},
+    protocol::Message,
+};
+
+type Sender = UnboundedSender<Message>;
+
+struct PeerStruct {
+    protocol: HeaderValue,
+    sender: Sender,
+}
+type PeerMap = Arc<Mutex<HashMap<SocketAddr, PeerStruct>>>;
 
 fn main() {
     // Get the port number to listen on (required for heroku deployment).
     let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
     let server_addr = format!("0.0.0.0:{}", port);
+
+    let state = PeerMap::new(Mutex::new(HashMap::new()));
 }
